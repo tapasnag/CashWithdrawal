6e09badc5fbd98588a1e7484467ebfcc2f20af88
tapasnag <tapas.nag76@gmail.com>
2022-12-19 07:03:56 +0000

    first commit

---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,24 @@
+// Write your code here
+import './index.css'
+
+const DenominationItem = props => {
+  const {denominationDetails, updateBalance} = props
+  const {value} = denominationDetails
+
+  const onDenomination = () => {
+    updateBalance(value)
+  }
+
+  return (
+    <li className="denomination-item">
+      <button
+        type="button"
+        className="denomination-button"
+        onClick={onDenomination}
+      >
+        {value}
+      </button>
+    </li>
+  )
+}
+export default DenominationItem
